cd453a0c4d15ac1a4e2b0962903c4534c006cf3c
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2021-12-05 16:51:59 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,4 +6,4 @@ echo root password is alpine;
 
 # su root password: alpine;
 
-dpkg -i "/var/mobile/Documents/MYDEB.deb";
+sudo dpkg -i "https://repo.packix.com/api/PackageVersions/603ceb0f0bd6490014283b05.deb";
